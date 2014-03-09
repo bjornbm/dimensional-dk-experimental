4fc61f155e684ce74507fc41078e241c0188b56b
Douglas McClean <douglas.mcclean@gmail.com>
2014-03-09 12:28:10 -0400

    Created a working version of UnitMap with customizable unit pretty-printing.

--- Numeric/Units/Dimensional/DK/UnitMap.hs
+++ Numeric/Units/Dimensional/DK/UnitMap.hs
@@ -9,40 +9,100 @@
 Define .  -}
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE Rank2Types #-}
 
 module Numeric.Units.Dimensional.DK.UnitMap where
 
-import Numeric.Units.Dimensional.DK.Prelude
+import Numeric.Units.Dimensional.DK.Prelude hiding (lookup)
+import Numeric.Units.Dimensional.DK.UnitNames (UnitName)
 import qualified Data.Map as M
+import Data.Proxy
+import Control.Monad (join)
+import Data.Maybe (fromMaybe)
 
 data AnyUnit v = AnyUnit Dimension' UnitName v
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Show)
 
-demote :: (KnownDimension d) => Unit a d v -> AnyUnit v
-demote = undefined
+demote :: forall a d v.(KnownDimension d, Fractional v) => Unit a d v -> AnyUnit v
+demote u = AnyUnit (toSIBasis (Proxy :: Proxy d)) (name u) (u /~ siUnit)
 
-promote :: AnyUnit v -> Maybe (Unit Composite d v)
-promote = undefined
+promote :: forall d v.(Fractional v, KnownDimension d) => AnyUnit v -> Maybe (Unit Composite d v)
+promote (AnyUnit dim name val) | dim == (toSIBasis (Proxy :: Proxy d)) = Just $ unit name (val *~ siUnit)
+                               | otherwise                             = Nothing
 
-newtype UnitMap v = UnitMap (M.Map Dimension' (AnyUnits v))
+newtype UnitMap v = UnitMap (M.Map Dimension' (AnyUnit v))
   deriving (Show)
 
 empty :: UnitMap v
 empty = UnitMap (M.empty)
 
-insert :: (KnownDimension d) => Unit a d v -> UnitMap v -> UnitMap v
-insertAny = insertAny . demote
+insert :: (KnownDimension d, Fractional v) => Unit a d v -> UnitMap v -> UnitMap v
+insert = insertAny . demote
 
 insertAny :: AnyUnit v -> UnitMap v -> UnitMap v
-insertAny u@(AnyUnit d v) (M.Map m) = M.Map (M.insert d u) m
+insertAny u@(AnyUnit d _ _) (UnitMap m) = UnitMap $ (M.insert d u) m
 
-lookup :: forall d v.(KnownDimension d) => UnitMap v -> Maybe (UnitInfo d v)
-lookup m = fmap promote $ lookupAny (toSIBasis (Proxy :: Proxy d)) m
+lookup :: forall d v.(KnownDimension d, Fractional v) => UnitMap v -> Maybe (Unit Composite d v)
+lookup m = join . fmap promote $ lookupAny (toSIBasis (Proxy :: Proxy d)) m
 
-lookupAny :: Dimension' -> UnitMap v -> AnyUnit v
-lookupAny d (M.Map m) = M.lookup d m
+lookupAny :: Dimension' -> UnitMap v -> Maybe (AnyUnit v)
+lookupAny d (UnitMap m) = M.lookup d m
 
-showUsing :: (KnownDimension d) => UnitMap v -> Quantity d v -> String
-showUsing m q = case (lookup m) of
-                  (Just u') -> showIn u' q
-                  Nothing -> show q
+type UnitScheme v = (forall d.(KnownDimension d, Fractional v) => Unit Composite d v)
+
+lookupWithDefault :: forall d v.(KnownDimension d, Fractional v) => UnitScheme v -> UnitMap v -> Unit Composite d v
+lookupWithDefault d m = fromMaybe d (lookup m)
+
+lookup' :: (KnownDimension d, Fractional v) => UnitMap v -> Unit Composite d v
+lookup' = lookupWithDefault siUnit
+
+showUsing :: (KnownDimension d, Show v, Fractional v) => UnitMap v -> Quantity d v -> String
+showUsing m q = showIn (lookup' m) q
+
+{-
+From table 3a of the NIST guide, copied from SIUnits module:
+
+> hertz :: Fractional a => Unit Atomic DFrequency a
+> newton :: Fractional a => Unit Atomic DForce a
+> pascal :: Fractional a => Unit Atomic DPressure a
+> joule :: Fractional a => Unit Atomic DEnergy a
+> watt :: Fractional a => Unit Atomic DPower a
+> coulomb :: Fractional a => Unit Atomic DElectricCharge a
+> volt :: Fractional a => Unit Atomic DElectricPotential a
+> farad :: Fractional a => Unit Atomic DCapacitance a
+> ohm :: Fractional a => Unit Atomic DElectricResistance a
+> siemens :: Fractional a => Unit Atomic DElectricConductance a
+> weber :: Fractional a => Unit Atomic DMagneticFlux a
+> tesla :: Fractional a => Unit Atomic DMagneticFluxDensity a
+> henry :: Fractional a => Unit Atomic DInductance a
+> lumen :: Fractional a => Unit Atomic DLuminousFlux a
+> lux :: Fractional a => Unit Atomic DIlluminance a
+
+-}
+
+exampleSI :: (Fractional v) => UnitMap v
+exampleSI = id
+          $ insert hertz
+          $ insert newton
+          $ insert pascal
+          $ insert joule
+          $ insert watt
+          $ insert coulomb
+          $ insert volt
+          $ insert farad
+          $ insert ohm
+          $ insert siemens
+          $ insert weber
+          $ insert tesla
+          $ insert henry
+          $ insert lumen
+          $ insert lux
+          $ insert candela
+          $ insert mole
+          $ insert kelvin
+          $ insert ampere
+          $ insert second
+          $ insert (kilo gram)
+          $ insert meter empty
