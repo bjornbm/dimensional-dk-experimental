fb6e2b4e93aafff6d80d87dfaa35071e99e89db8
Douglas McClean <douglas.mcclean@gmail.com>
2015-10-06 17:24:49 -0400

    Updated unit map module to use new named units approach.

--- src/Numeric/Units/Dimensional/DK/UnitMap.hs
+++ src/Numeric/Units/Dimensional/DK/UnitMap.hs
@@ -8,47 +8,49 @@
 
 Define .  -}
 
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Numeric.Units.Dimensional.DK.UnitMap where
 
 import Numeric.Units.Dimensional.DK.Prelude hiding (lookup)
 import Numeric.Units.Dimensional.DK.Dynamic
 import qualified Data.Map as M
+import Data.ExactPi
 import Data.Proxy
 import Data.Maybe (fromMaybe)
 
-newtype UnitMap v = UnitMap (M.Map Dimension' (AnyUnit v))
+newtype UnitMap = UnitMap (M.Map Dimension' AnyUnit)
   deriving (Show)
 
-empty :: UnitMap v
+empty :: UnitMap
 empty = UnitMap (M.empty)
 
-insert :: (KnownDimension d, Fractional v) => Unit a d v -> UnitMap v -> UnitMap v
+insert :: (KnownDimension d) => Unit a d v -> UnitMap -> UnitMap
 insert = insertAny . demoteUnit
 
-insertAny :: AnyUnit v -> UnitMap v -> UnitMap v
-insertAny u (UnitMap m) = UnitMap $ (M.insert (getSIBasis u) u) m
+insertAny :: AnyUnit -> UnitMap -> UnitMap
+insertAny u (UnitMap m) = UnitMap $ (M.insert (dimension u) u) m
 
-lookup :: forall d v.(KnownDimension d, Fractional v) => UnitMap v -> Maybe (Unit NotPrefixable d v)
+lookup :: forall d.(KnownDimension d) => UnitMap -> Maybe (Unit 'NonMetric d ExactPi)
 lookup m = lookupAny dim m >>= promoteUnit
-         where dim = toSIBasis (Proxy :: Proxy d)
+         where dim = dimension (Proxy :: Proxy d)
 
-lookupAny :: Dimension' -> UnitMap v -> Maybe (AnyUnit v)
+lookupAny :: Dimension' -> UnitMap -> Maybe AnyUnit
 lookupAny d (UnitMap m) = M.lookup d m
 
-type UnitScheme v = (forall d.(KnownDimension d, Fractional v) => Unit NotPrefixable d v)
+type UnitScheme v = (forall d.(KnownDimension d, Floating v) => Unit 'NonMetric d v)
 
-lookupWithDefault :: forall v.(Fractional v) => UnitScheme v -> UnitMap v -> UnitScheme v
-lookupWithDefault d m = fromMaybe d (lookup m)
+lookupWithDefault :: forall v.(Floating v) => UnitScheme v -> UnitMap -> UnitScheme v
+lookupWithDefault d m = fromMaybe d (fmap changeRepApproximate $ lookup m)
 
-lookup' :: (Fractional v) => UnitMap v -> UnitScheme v
+lookup' :: (Floating v) => UnitMap -> UnitScheme v
 lookup' = lookupWithDefault siUnit
 
-showUsing :: (KnownDimension d, Show v, Fractional v) => UnitScheme v -> Quantity d v -> String
+showUsing :: (KnownDimension d, Show v, Floating v) => UnitScheme v -> Quantity d v -> String
 showUsing scheme = showIn scheme -- for some reason this won't type check if you eta convert?
 -- this and UnitScheme should probably migrate to dimensional-dk
 
@@ -73,7 +75,7 @@ From table 3a of the NIST guide, copied from SIUnits module:
 
 -}
 
-exampleSI :: (Fractional v) => UnitMap v
+exampleSI :: (Fractional v) => UnitMap
 exampleSI = id
           $ insert hertz
           $ insert newton
